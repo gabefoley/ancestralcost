8d5697b00ecacfad5921f778c51de1fb67fc4d2e
gabefoley <gabriel.foley@uqconnect.edu.au>
2021-09-23 17:06:12 +1000

    Example runs

--- ancestralcost/__main__.py
+++ ancestralcost/__main__.py
@@ -1,159 +1,25 @@
 """ Check for each position in a given ancestor that the presence of ancestral content implied to be there by a given alignment and tree is not substantially less parsimonious then the alternative of not having ancestral content there."""
 
 import argparse
+import sys
 
 from ete3 import PhyloTree
 import ancestralcost.indel_placement as indel_placement
+import ancestralcost.tree_code as tree_code
+
 from Bio import AlignIO
 
-__version__ = "1.0.4"
+from ancestralcost import __version__
 
+def run(args):
+    print(f"\U0001F4B0" + " Running Ancestral Cost v" + __version__ + " \U0001F4B0" + "\n")
 
-def load_tree_with_alignment(tree, alignment):
-    """Load a tree and associate it with a specific alignment"""
-    tree = PhyloTree(tree, alignment=alignment, format=1, alg_format="fasta")
-    return tree
-
-
-def get_positions_with_content(tree, alignment, node_name="#N0"):
-    """
-    Return the alignment positions that must be present in the ancestor at the given node
-    :param tree: The phylogenetic tree
-    :param node: The node of interest (defaults to root node)
-    :return:
-    """
-
-    selected_node = None
-
-    # Get the node of interest
-    if node_name in ("#N0", "N0"):
-        selected_node = tree.get_tree_root()
-
-    else:
-        for node in tree.get_descendants():
-            if node.name == node_name:
-                selected_node = node
-                break
-
-        if selected_node is None:
-            raise NameError("The node you selected is not in this tree")
-
-    # Check each position in the alignment to see if any leaf in both children have content there
-    # If a position appears in each child tree, no need to check it at higher nodes - it must appear at this node
-    found_positions = []
-
-    # If a position appears in neither child tree, no need to check it at higher nodes - it won't appear at this node
-    skip_positions = []
-
-    found_positions = sorted(
-        get_found_positions(
-            selected_node, len(alignment[0]), found_positions, skip_positions
-        )
-    )
-
-    return found_positions
-
-
-def get_found_positions(selected_node, aln_len, found_positions, skip_positions):
-    """Just return the positions that are implied to be there"""
-
-    # Get the leaf nodes under each child subtree
-    child1_leaves = selected_node.children[0].get_leaves()
-    child2_leaves = selected_node.children[1].get_leaves()
-
-    for pos in range(aln_len):
-        if pos not in found_positions and pos not in skip_positions:
-            found1 = False
-            found2 = False
-            for leaf in child1_leaves:
-                if leaf.sequence[pos] != "-":
-                    found1 = True
-                    break
-            for leaf in child2_leaves:
-                if leaf.sequence[pos] != "-":
-                    found2 = True
-                    break
-
-            # Must appear (no need to search in higher nodes)
-            if found1 and found2:
-                found_positions.append(pos)
-
-            # Doesn't appear (no need to search in higher nodes)
-            elif not found1 and not found2:
-                skip_positions.append(pos)
-
-    if selected_node.is_root() or len(found_positions) + len(skip_positions) == aln_len:
-        return found_positions
-
-    else:
-        return get_found_positions(
-            selected_node.up, aln_len, found_positions, skip_positions
-        )
-
-
-def get_parsimony_table(tree):
-    """Create the table that holds the parsimony scores"""
-    print("Making total parsimony\n")
-    parsimony = indel_placement.make_total_parsimony(tree)
-    print("Sorting parsimony table\n")
-    parsimony.sort_index(inplace=True)
-
-    return parsimony
-
-
-def get_parsimony_score(parsimony, node, pos, unbalanced=True):
-    """Get a parsimony score for a specific position"""
-    score = parsimony[pos][node]
-
-    # Only return the score if cost for presence is higher than cost for absence
-    if unbalanced:
-        if score[0] > score[1]:
-            return score
-
-    else:
-        return score
-
-
-def get_parsimony_scores(parsimony, positions, node="#N0"):
-    """Get parsimony scores for all positions in a given node"""
-    parsimony_dict = {}
-    for pos in positions:
-        score = get_parsimony_score(parsimony, node, pos)
-        if score:
-            parsimony_dict[pos] = score
-    return parsimony_dict
-
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-
-    parser.add_argument("-a", "--aln", help="Path to alignment", required=True)
-    parser.add_argument("-t", "--tree", help="Path to phylogenetic tree", required=True)
-    parser.add_argument(
-        "-n", "--node", help="Node to return cost for (default is root)"
-    )
-    parser.add_argument(
-        "-p",
-        "--positions",
-        help="Just return the positions required to be there",
-        action="store_true",
-    )
-    parser.add_argument(
-        "-f", "--fasta_output", help="Return all ancestors as a FASTA file"
-    )
-    parser.add_argument("-to", "--tree_output", help="Write out the ancestor tree")
-
-    args = parser.parse_args()
-
-    # node_of_interest = 0 if not args.node else args.node
     node_of_interest = "#N0" if not args.node else args.node
-    print()
 
-    print("\U0001F4B0" + " Running Ancestral Cost " + "\U0001F4B0" + "\n")
 
     aln = AlignIO.read(args.aln, "fasta")
 
-    tree = load_tree_with_alignment(args.tree, args.aln)
+    tree = tree_code.load_tree_with_alignment(args.tree, args.aln)
 
     if args.fasta_output:
         print("Getting the positions required to be there for all ancestors")
@@ -161,7 +27,7 @@ if __name__ == "__main__":
             for node in tree.traverse():
                 if not node.is_leaf():
                     print(node.name)
-                    positions = get_positions_with_content(tree, aln, node.name)
+                    positions = tree_code.get_positions_with_content(tree, aln, node.name)
                     print(positions)
                     print(len(aln[0]))
 
@@ -179,7 +45,7 @@ if __name__ == "__main__":
     else:
 
         print("Getting the positions that are required to be there\n")
-        positions = get_positions_with_content(tree, aln, node_of_interest)
+        positions = tree_code.get_positions_with_content(tree, aln, node_of_interest)
 
         print(
             "These positions are required to be there - ",
@@ -197,10 +63,10 @@ if __name__ == "__main__":
             )
 
             print("Getting the parsimony scores\n")
-            parsimony = get_parsimony_table(tree)
+            parsimony = tree_code.get_parsimony_table(tree)
 
             print("Getting the parsimony scores for required positions\n")
-            parsimony_scores = get_parsimony_scores(
+            parsimony_scores = tree_code.get_parsimony_scores(
                 parsimony, positions, node_of_interest
             )
 
@@ -219,3 +85,45 @@ if __name__ == "__main__":
                         f" being there which was {v[1]}"
                     )
                     print()
+
+
+
+def ac_parser():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("-a", "--aln", help="Path to alignment", required=True)
+    parser.add_argument("-t", "--tree", help="Path to phylogenetic tree", required=True)
+    parser.add_argument(
+        "-n", "--node", help="Node to return cost for (default is root)"
+    )
+    parser.add_argument(
+        "-p",
+        "--positions",
+        help="Just return the positions required to be there",
+        action="store_true",
+    )
+    parser.add_argument(
+        "-f", "--fasta_output", help="Return all ancestors as a FASTA file"
+    )
+    parser.add_argument("-to", "--tree_output", help="Write out the ancestor tree")
+
+    return parser
+
+
+def main(args=None):
+    parser = ac_parser()
+    if args:
+        sys.argv = args
+
+    if len(sys.argv) > 1 and sys.argv[1] in {'-v', '--v', '-version', '--version'}:
+        print(f'ancestralcost v{__version__}')
+        sys.exit(0)
+    else:
+        print(f'ancestralcost v{__version__}')
+        args = parser.parse_args(args)
+        run(args)
+    sys.exit(0)
+
+
+if __name__ == "__main__":
+    main()
